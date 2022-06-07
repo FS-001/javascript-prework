8cc728ea1e7c3ade133ff2b4bf0eddaa20b9f0ca
Jarek Doliński <jaroslaw.dolinski@hotmail.com>
2022-06-07 20:34:35 +0200

    Add curly brackets for the whole script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+{
 function playGame(playerInput){
 
     clearMessages();
@@ -59,3 +60,4 @@ function playGame(playerInput){
 document.getElementById('play-rock').addEventListener('click', function(){playGame(1);});
 document.getElementById('play-paper').addEventListener('click', function(){playGame(2);});
 document.getElementById('play-scissors').addEventListener('click', function(){playGame(3);});
+}
